7703af3080d496d730bb53a918c88f9057cdb603
theaderonkeadebisi <theaderonkeadebisi@gmail.com>
2023-07-14 16:15:10 -0700

    Smile in the mirror

---
+++ 0x01-variables_if_else_while/7-tebahpla.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main - Program entry point
+ *
+ * Return: 0 on success. Error code otherwise
+ */
+int main(void)
+{
+	char ch ='z';
+
+	while(ch >= 'a')
+	{
+		putchar(ch);
+		ch--;
+	}
+	putchar('\n');
+	return 0;
+}

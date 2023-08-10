8ebec492685b594ccb692812224f733ebe0f5931
theaderonkeadebisi <theaderonkeadebisi@gmail.com>
2023-08-10 08:19:11 -0700

    string_nconcat

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -9,15 +9,15 @@
  */
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
-	int i;
-	int s1len = 0;
-	int s2len = 0;
+	unsigned int i;
+	unsigned int s1len = 0;
+	unsigned int s2len = 0;
 	char *output;
 
 	if (s1 == NULL)
 		s1 = "";
 	if (s2 == NULL)
-		S2 = "";
+		s2 = "";
 	for (i = 0; s1[i] != '\0'; i++)
 		s1len++;
 	for (i = 0; s2[i] != '\0'; i++)
